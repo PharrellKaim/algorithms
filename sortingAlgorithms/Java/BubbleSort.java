bcc3b7366466d774f8eb74bd5cda8ee02d18ecf3
Pharrell Kaim <79710229+PharrellKaim@users.noreply.github.com>
2024-01-16 19:41:58 +0100

    edited

--- sortingAlgorithms/Java/BubbleSort.java
+++ sortingAlgorithms/Java/BubbleSort.java
@@ -8,15 +8,15 @@ public class BubbleSort {
             for(int j = 1; j < (length-i); j++){
                 if(array[j-1] > array[j]) {
                     //swap the elements
-                    temp = array[i];
-                    array[i] = array[j];
+                    temp = array[j-1];
+                    array[j-1] = array[j];
                     array[j] = temp;
                 }
             }
         }
     }
     public static void main(String[] args){
-        int[] array = {2,4,1,8};
+        int[] array = {2,4,1,8,6, 7 , 90, 70, 20, 50};
         bubbleSort(array);
         for (int i = 0; i < array.length; i++){
             System.out.println(array[i]);
